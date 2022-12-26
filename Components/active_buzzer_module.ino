e5828d88a9876204066976e58a0f98fb232b0dc1
Daniel-Ian-Robinson <92424879+Daniel-Ian-Robinson@users.noreply.github.com>
2022-12-27 12:04:06 +1300

    Drafted active buzzer

---
+++ Components/active_buzzer_module.ino
@@ -0,0 +1,12 @@
+const int BUZZER_PIN = 7;
+
+void setup() {
+    pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(BUZZER_PIN, HIGH);
+    delay(1000);
+    digitalWrite(BUZZER_PIN, LOW);
+    delay(1000);
+}
