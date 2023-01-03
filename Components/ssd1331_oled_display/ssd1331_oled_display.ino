41b79a089b4074e98fe63bba8a01b4fdc6294b52
Daniel-Ian-Robinson <daniel.robinson.21.dr@gmail.com>
2023-01-03 23:12:29 +1300

    Removed shapes form SSD1331 screen code

--- Components/ssd1331_oled_display/ssd1331_oled_display.ino
+++ Components/ssd1331_oled_display/ssd1331_oled_display.ino
@@ -13,9 +13,6 @@
 
 #define BLACK   0x0000
 #define WHITE   0xFFFF
-#define RED     0xF800
-#define GREEN   0x07E0
-#define BLUE    0x001F
 
 Adafruit_SSD1331 display = Adafruit_SSD1331(CS_PIN, DC_PIN, RES_PIN);
 
@@ -24,42 +21,19 @@ void setup() {
 
     // Draw text.
     display.fillScreen(BLACK);
-    display.setCursor(0, 0);
-    display.print("Hello world!");
+    display.setCursor(15, 25);
+    display.println("Hello world!");
     delay(3000);
 
-    // Draw pixels.
+    // Draw randomly coloured pixels in random places.
     display.fillScreen(BLACK);
-    for (int i = 0; i < 30; i++) {
+    for (int i = 0; i < 120; i++) {
         int xVal = random(0, SCREEN_WIDTH);
         int yVal = random(0, SCREEN_HEIGHT);
-        display.drawPixel(xVal, yVal, WHITE);
-        delay(100);
+        int colour = random(BLACK, WHITE);
+        display.drawPixel(xVal, yVal, colour);
+        delay(50);
     }
-
-    // Draw lines.
-    display.fillScreen(BLACK);
-    display.drawLine(0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, RED);
-    delay(300);
-    display.drawLine(20, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, RED);
-    delay(300);
-    display.drawLine(0, 20, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1, RED);
-    delay(3000);
-
-    // Draw empty rectangle.
-    display.fillScreen(BLACK);
-    display.drawRect(15, 15, 30, 30, GREEN);
-    delay(3000);
-    
-    // Draw filled rectangle.
-    display.fillScreen(BLACK);
-    display.fillRect(15, 15, 30, 30, GREEN);
-    delay(3000);
-    
-    // Draw circle.
-    display.fillScreen(BLACK);
-    display.drawCircle(SCREEN_WIDTH / 2 - 1, SCREEN_HEIGHT / 2 - 1, 30, BLUE);
-    delay(3000);
 }
 
 void loop() {}
